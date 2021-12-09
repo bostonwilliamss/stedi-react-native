ff6247e574a5b5ef025df19bd94b2feeb040e9b1
bostonwilliamss <91291999+bostonwilliamss@users.noreply.github.com>
2021-12-09 16:13:13 -0700

    Update App.js

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Counter from './Counter.js';
+import Timer from './Timer.js';
 import SettingsScreen from './SettingsScreen.js';
 import Home from './Home.js';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import screen from './screen.js'; 
 
 // import Icons from "./Icons";
 const Tab = createMaterialBottomTabNavigator();
@@ -16,7 +17,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        initialRouteName='Home'
+        initialRouteName='Login'
         activeColor='white'
         barStyle={{ backgroundColor: 'green' }}
       >
@@ -31,12 +32,12 @@ export default function App() {
           }}
         />
         <Tab.Screen
-          name='Step Counter'
-          component={Counter}
+          name='Timer'
+          component={Timer}
           options={{
-            tabBarLabel: 'Step Counter',
+            tabBarLabel: 'Timer',
             tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name='watch' color={color} size={26} />
+              <MaterialCommunityIcons name='bell' color={color} size={26} />
             ),
           }}
         />
@@ -50,6 +51,17 @@ export default function App() {
             ),
           }}
         />
+ <Tab.Screen
+          name='Screen'
+          component={screen}
+          options={{
+            tabBarLabel: 'Screen',
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name='gear' color={color} size={26} />
+            ),
+          }}
+        />
+
       </Tab.Navigator>
     </NavigationContainer>
   );
