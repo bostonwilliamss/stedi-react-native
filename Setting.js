0f98270910c7c034c570c3f3eb3ba1e44b6b2398
bostonwilliamss <91291999+bostonwilliamss@users.noreply.github.com>
2021-12-09 16:12:24 -0700

    Update Setting.js

--- Setting.js
+++ Setting.js
@@ -1,18 +1,25 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput} from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-
 
 function Setting() {
+  const [button_clicked, setButtonClicked] = useState(false);
+  if (button_clicked) {
+    return(
+      <View>
+      <Timer>
+      </Timer>
+      </View>
+          )
+  }
+  else {
     return(
       <View>
       <Button
-      title="Log In"
-      onPress={() => Alert.alert('Simple Button pressed')}
+      title="Login"
+      onPress={() => {setButtonClicked(true)}}
     />
       </View>
     )
   }
-
-export default Setting;
+}
